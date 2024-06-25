df4eed063172d65fc29c7bf0add5b3362adce9a8
Tharindi-Bandara <bandaragkt.21@uom.lk>
2024-06-26 02:15:24 +0530

    Update models.py- booking model

--- Car-Rental-System/Car-Rental-System/MyApp/models.py
+++ Car-Rental-System/Car-Rental-System/MyApp/models.py
@@ -17,3 +17,26 @@ class Car(models.Model):
 
     def __str__(self):
         return self.car_name
+
+class Booking(models.Model):
+    vehicle = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='bookings')
+    customer_name = models.CharField(max_length=30, default="")
+    customer_phone = models.CharField(max_length=15, default="")
+    nic = models.CharField(max_length=20, default="")  # New field for NIC
+    pickup_date = models.DateField()
+    return_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.vehicle.car_name} - {self.pickup_date} to {self.return_date}"
+
+    def save(self, *args, **kwargs):
+        # Check for overlapping bookings
+        overlapping_bookings = Booking.objects.filter(vehicle=self.vehicle).filter(
+            models.Q(pickup_date__range=[self.pickup_date, self.return_date]) |
+            models.Q(return_date__range=[self.pickup_date, self.return_date])
+        ).exclude(pk=self.pk)
+        if overlapping_bookings.exists():
+            raise ValueError("This vehicle is already booked for the given dates.")
+
+        super().save(*args, **kwargs)
