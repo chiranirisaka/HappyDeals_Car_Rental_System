468411d5185b8658d712927aa955d25a0b518ec1
Tharindi-Bandara <bandaragkt.21@uom.lk>
2024-06-26 02:16:16 +0530

    Update models.py-contact model

--- Car-Rental-System/Car-Rental-System/MyApp/models.py
+++ Car-Rental-System/Car-Rental-System/MyApp/models.py
@@ -40,3 +40,15 @@ class Booking(models.Model):
             raise ValueError("This vehicle is already booked for the given dates.")
 
         super().save(*args, **kwargs)
+
+
+class Contact(models.Model):
+    message = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=150,default="")
+    email = models.CharField(max_length=150,default="")
+    phone_number = models.CharField(max_length=15,default="")
+    message = models.TextField(max_length=500,default="")
+
+    def __str__(self) :
+        return self.name
+
